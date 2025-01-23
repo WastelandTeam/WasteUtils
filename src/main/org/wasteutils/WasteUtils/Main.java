8654a9343712c93358bd5b2482094647355ade1e
xiaodi001-01 <3292744510@qq.com>
2025-01-23 16:43:37 +0800

    Added config.yml to control further feature.

--- src/main/org/wasteutils/WasteUtils/Main.java
+++ src/main/org/wasteutils/WasteUtils/Main.java
@@ -1,7 +1,24 @@
 package org.wasteutils.WasteUtils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        this.saveDefaultConfig();
+        this.reloadConfig();
+        Bukkit.getConsoleSender().sendMessage("§3WasteUtils§f >>§ePlugin was loaded. Hello from WasteUtils V0.0.1 Demo!");
+    }
 
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§3WasteUtils§f >>§ePlugin was Enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("§3WasteUtils§f >>§ePlugin was Disabled.");
+    }
 }
